ec9414ac207ddd05b2baae605f734280ee80d3a8
meta <meta@meta.local>
2019-04-26 21:27:13 +0800

    go

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt-get install python3-pip supervisor python3.7 -y
 python3.7 /usr/bin/pip3 install aiohttp
 
-cd /root/hades/hades
+cd /root/hades
 cp hades.conf /etc/supervisor/conf.d
 supervisorctl reload
